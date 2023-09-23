ada32d08af7c4e178c5c661ce0ba4b7a1e7e2d49
Alyssa Dong <105294624+alyssadong2006@users.noreply.github.com>
2023-09-23 17:53:56 -0400

    Delete blog directory

--- blog/apps.py
+++
@@ -1,7 +0,0 @@
-"""create blog app"""
-from django.apps import AppConfig
-
-class BlogConfig(AppConfig):
-    """blog app"""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
